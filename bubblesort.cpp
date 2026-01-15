6cc3339c1f8340ce8d1019fa1b2401480da6c5bc
Vaibhav01-bit <vi1804365@gmail.com>
2026-01-15 12:11:44 +0530

    bubble sort

---
+++ bubblesort.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std ;
+
+
+void print (int arr[], int n){
+    for(int i =0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+void bublesort(int arr[], int n){
+    for(int i=0;i<n-1;i++){
+        for (int j= 0;j<n-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j] , arr [j+1]);
+            }
+             
+            }
+        }
+        print(arr, n);
+    }
+   
+
+
+int main() {
+    // Write C++ code here
+    int arr[5]={5,4,1,3,2};
+    
+    bublesort(arr,5);
+
+    return 0;
+}
